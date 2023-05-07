8285eb700be1495370270ba2c759eeb87e8ebdb9
JOVANI <david.alejandro16ca@gmail.com>
2023-05-07 17:07:23 -0500

    completado

---
+++ entregableUno.java
@@ -0,0 +1,34 @@
+package Modulo2;
+public class entregableUno {
+    public static void main(String[] args) {
+
+        //numeros
+        byte Byte = 1;
+        System.out.println("Tipo dato byte : " + Byte);
+        short Short = 10;
+        System.out.println("Tipo dato Short : " + Short);
+        int Int = 100;
+        System.out.println("Tipo dato int : " + Int);
+        long Long = 10000;
+        System.out.println("Tipo dato long : " + Long);
+
+        //decimal
+        float Float = 10.9f;
+        System.out.println("Tipo dato float : " + Float);
+        double Double = 10.9d;
+        System.out.println("Tipo dato double : " + Double);
+
+        //boolean
+        boolean True = true;
+        System.out.println("Tipo dato boolean : " + True);
+        boolean False = false;
+        System.out.println("Tipo dato boolean : "+ False);
+
+        //textos
+        char Char = 'c';
+        System.out.println("Tipo dato char : " + Char);
+        String string = "Hola Mundo";
+        System.out.println("Tipo dato String : " + string);
+
+    }
+}
